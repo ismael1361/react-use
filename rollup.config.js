234be4f12436b30dbeeafc029aa3e165090e7e93
Ismael Souza Silva <ismael1361@gmail.com>
2025-10-09 20:01:49 -0300

    build: add external dependencies
    - Use package.json dependencies
    - Externalize dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -2,31 +2,34 @@ const resolve = require("@rollup/plugin-node-resolve");
 const commonjs = require("@rollup/plugin-commonjs");
 const typescript = require("@rollup/plugin-typescript");
 const terser = require("@rollup/plugin-terser");
+const current_package = require("./package.json");
+
+const dependencies = Object.keys(current_package?.dependencies || {});
 
 module.exports = {
-    input: "src/index.ts",
-    output: [
-        {
-            file: "dist/index.js",
-            format: "cjs",
-            sourcemap: true,
-        },
-        {
-            file: "dist/index.esm.js",
-            format: "esm",
-            sourcemap: true,
-        },
-    ],
-    plugins: [
-        resolve(),
-        commonjs(),
-        typescript({
-            tsconfig: "./tsconfig.json",
-            declaration: true,
-            declarationDir: "./dist",
-            sourceMap: true,
-        }),
-        terser(),
-    ],
-    external: ["react", "react-dom"],
+	input: "src/index.ts",
+	output: [
+		{
+			file: "dist/index.js",
+			format: "cjs",
+			sourcemap: true,
+		},
+		{
+			file: "dist/index.esm.js",
+			format: "esm",
+			sourcemap: true,
+		},
+	],
+	plugins: [
+		resolve(),
+		commonjs(),
+		typescript({
+			tsconfig: "./tsconfig.json",
+			declaration: true,
+			declarationDir: "./dist",
+			sourceMap: true,
+		}),
+		terser(),
+	],
+	external: dependencies,
 };
